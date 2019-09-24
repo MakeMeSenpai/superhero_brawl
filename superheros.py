c5a414274297fa05778e2b9c67cd3aeaed8f8e85
Anthony <sean.a.protho1@gmail.com>
2019-09-24 16:24:43 -0700

    Fixed tests

--- superheros.py
+++ superheros.py
@@ -1,33 +1,22 @@
 import random
 
 class Ability():
-    def attack(self):
-        def __init__(self, name, attack_strength):
-            self.name = name #str
-            self.max_damage = 20 #attack_strength #int
+    def __init__(self, name, attack_strength):
+        self.name = name #str
+        self.max_damage = 20 #attack_strength #int
 
             #creates random attack value
-            def attack(self):
-                attack_value = random.randint(0,self.max_damage)
-                return attack_value
-
-        #TODO: Fix abilities test, this a requirement, this applies to all functions :/
-        # if __name__ == "__main__":
-        # # If you run this file from the terminal
-        # # this block is executed.
-        # ability = Ability("Debugging Ability", 20)
-        # print(ability.name)
-        # print(ability.attack())
+    def attack(self):
+        return random.randint(0,self.max_damage)
 
 class Armor():
     def __init__(self, name, max_block):
         self.name = name #str
         self.max_block = 20 #max_block #int
 
-        #creates random block value
-        def block(self):
-            block_value = random.randint(0, self.max_block)
-            return block_value
+    #creates random block value
+    def block(self):
+        return random.randint(0, self.max_block)
 
 class Hero():
     def __init__(self, name, starting_health=100):
@@ -36,41 +25,39 @@ class Hero():
         self.current_health = 100 #int
         self.abilities = []
         self.armors= []
+    
+    def add_ability(self, ability):
+        new_ability = input("What's your new ability? ")
+        return self.abilities.append(new_ability)
+
+    #TODO: Create The Attack Method- func should interate over abilities list calling Ability.attack() 
+    #for each ability in the list. Than add together all the randint values together to determine full
+    #damage. (test is already included)
+    def attack(self):
+        count = 0
+        for num in self.abilities:
+            count += 1 
+        return Ability.attack(count)
+
+    def defend(self, incoming_damage):
         pass
 
-        # TODO: Just another test to fix
-        # if __name__ == "__main__":
-        # # If you run this file from the terminal
-        # # this block is executed.
-        # my_hero = Hero("Grace Hopper", 200)
-        # print(my_hero.name)
-        # print(my_hero.current_health)
+    def take_damage(self):
+        pass
 
-        def add_ability(self, ability):
-            new_ability = input("What's your new ability? ")
-            self.abilities.append(new_ability)
-            return self.abilities
+    def is_alive(self):
+        pass
 
-        # TODO: And another test
-        # if __name__ == "__main__":
-        # # If you run this file from the terminal
-        # # this block is executed.
-        # ability = Ability("Great Debugging", 50)
-        # hero = Hero("Grace Hopper", 200)
-        # hero.add_ability(ability)
-        # print(hero.abilities)
+    def fight(self, opponent): #opponent == hero class
+        pass
 
-        def attack(self):
-            pass
-
-        def defend(self, incoming_damage):
-            pass
-
-        def take_damage(self):
-            pass
-
-        def is_alive(self):
-            pass
-
-        def fight(self, opponent): #opponent == hero class
-            pass
+#tests your code!
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
